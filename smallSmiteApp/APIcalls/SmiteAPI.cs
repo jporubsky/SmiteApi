23c230ad34892916382a3d21ac0b1a128e3b49ae
punkysmurf <punk_e_smurf@yahoo.com>
2017-08-08 01:35:59 -0400

    remote creation

---
+++ smallSmiteApp/APIcalls/SmiteAPI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Windows;
+
+namespace smallSmiteApp.APIcalls
+{
+    public class SmiteAPI
+    {
+        string sessionID;
+        string DevID = "2193";
+        string AuthKey = "A94348A93B544511952BAA38CE96CA8B";
+        string baseURL = "http://api.smitegame.com/smiteapi.svc/";
+        public SmiteAPI()
+        {
+                
+        }
+
+        public string ApiCall(string urlParameters)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(baseURL);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/xml"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call!
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body. Blocking!
+                return response.Content.ReadAsStringAsync().Result;
+                
+            }
+            else
+            {
+                return (int)response.StatusCode + response.ReasonPhrase;
+            }
+        }
+
+        public string Ping()
+        {
+            string retValue= baseURL + "pingxml";
+            return retValue;
+        }
+    }
+}
